6088a1c2e1fc74d1d23186ef587576b0330154ec
Jleagle <jimeagle@gmail.com>
2015-01-22 21:34:38 +0000

    Add character response

---
+++ src/Responses/Warcraft/CharacterResponse.php
@@ -0,0 +1,35 @@
+<?php
+namespace Jleagle\BattleNet\Responses\Warcraft;
+
+use Jleagle\BattleNet\Responses\BaseResponse;
+
+class CharacterResponse extends BaseResponse
+{
+  public $lastModified;
+  public $name;
+  public $realm;
+  public $battlegroup;
+  public $characterClass;
+  public $race;
+  public $gender;
+  public $level;
+  public $achievementPoints;
+  public $thumbnail;
+  public $calcClass;
+  public $guild;
+  public $feed;
+  public $items;
+  public $stats;
+  public $reputation;
+  public $titles;
+  public $achievements;
+  public $talents;
+  public $appearance;
+  public $mounts;
+  public $pets;
+  public $petSlots;
+  public $progression;
+  public $pvp;
+  public $quests;
+  public $audit;
+}
